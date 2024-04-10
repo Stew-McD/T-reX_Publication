6504aecd984b28e6a04b32a0a4c21b74fdd2ad8e
Stew-McD <sc.mcdowall@cml.leidenuniv.nl>
2024-04-10 17:33:26 +0200

    README update

--- README.md
+++ README.md
@@ -2,21 +2,17 @@
 
 This repo contains the manuscript and data associated with the T-reX program for LCA (currently a draft under internal review).
 
-* See the current version of the manuscript [here](https://github.com/Stew-McD/T-reX_Publication/blob/main/reviews/T-reX_Manuscript_V2/T-reX_manuscript_V3.pdf)
-  
-* Commented versions from reviewers are in the `reviews` directory.
+- See the current version of the manuscript [here](https://github.com/Stew-McD/T-reX_Publication/blob/main/00_JRR-submission_folder/00_T-reX_manuscript.pdf)
+- Commented draft versions from are in the [reviews](https://github.com/Stew-McD/T-reX_Publication/blob/main/reviews) directory.
+- Comments and insults are most welcome. If they are short, use the 'issues' tab or email me, otherwise, please annotate the pdf and send it back to me. Thanks!! x
+- Drafted according to the submission guidelines for the journal of Resources Conservation and Recycling [(see summary here)](ResourcesConservationAndRecycling_SubmissionRequirements.pdf)
 
-* Comments and insults are most welcome. If they are short, use the 'issues' tab or email me, otherwise, please annotate the pdf and send it back to me. Thanks!! x
-
-* Drafted according to the submission guidelines for the journal of Resources Conservation and Recycling [(see summary here)](ResourcesConservationAndRecycling_SubmissionRequirements.pdf)
-
+<!-- https://github.com/Stew-McD/T-reX_Publication/blob/main/00_JRR-submission_folder/00_T-reX_manuscript.pdf -->
 
 ![T-reX logo](logos/T-reX_logo_500.webp)
-
 
 ## Visual overview of T-reX
 
 ### Generalised and computational flowcharts
 
-<img src="T-reX_flowchart_combined.png" alt="T-reX flowcharts" width="500">
-
+<img src="latex_files/figures/T-reX_method.pdf" alt="T-reX flowcharts" width="500">
--- latex_files/.compile_pdf.sh
+++ latex_files/.compile_pdf.sh
@@ -3,41 +3,46 @@
 # Define paths and filenames
 source_folder="." # Assuming your LaTeX files are in the current directory
 target_folder="../00_JRR-submission_folder"
-main_tex_file="main.tex" # Name of your main LaTeX file
-bib_file="references.bib" # Name of your bibliography file
-output_pdf_name="00_T-reX_manuscript.pdf" # Desired name for the output PDF
+main_tex_file="main.tex"                    # Name of your main LaTeX file
+bib_file="references.bib"                   # Name of your bibliography file
+output_pdf_name="00_T-reX_manuscript.pdf"   # Desired name for the output PDF
+output_docx_name="00_T-reX_manuscript.docx" # Desired name for the output DOCX
+output_html_name="00_T-reX_manuscript.html" # Desired name for the output HTML
+
 echo "======================================================="
-echo "Compiling $main_tex_file to $output_pdf_name"
+echo "Compiling $main_tex_file to $output_pdf_name, $output_docx_name, and $output_html_name"
+
 # Create target folder if it doesn't exist
 mkdir -p "$target_folder"
-echo "======================================================="
-latexmk -c $main_tex_file || exit 1
-latexmk -pdf $main_tex_file || exit 1
 
 # Clean up before compiling
-# pdflatex -c "$main_tex_file" || exit 1
+latexmk -c $main_tex_file || exit 1
 
-# # Compile LaTeX document
-# pdflatex -interaction=batchmode -file-line-error "$main_tex_file"
-# bibtex "${main_tex_file%.*}" # Run bibtex if needed
-# pdflatex -interaction=batchmode -file-line-error "$main_tex_file"
-# pdflatex -interaction=batchmode -file-line-error "$main_tex_file" # Run twice for references
+# Compile LaTeX document to PDF
+latexmk -pdf $main_tex_file || exit 1
 
 # Check if PDF was generated
 if [ ! -f "${main_tex_file%.*}.pdf" ]; then
-    echo "PDF generation failed."
-    exit 1
+	echo "PDF generation failed."
+	exit 1
 fi
+
+# Convert PDF to DOCX
+pandoc -s "${main_tex_file%.*}.pdf" -o "$target_folder/$output_docx_name" || exit 1
+
+# Convert PDF to HTML
+pandoc -s "${main_tex_file%.*}.pdf" -o "$target_folder/$output_html_name" || exit 1
 
 # Move the PDF and .bib to the target folder
 cp "${main_tex_file%.*}.pdf" "$target_folder/$output_pdf_name"
 cp "$source_folder/$bib_file" "$target_folder/T-reX_references.bib"
 
 # Clean up temporary files
-rm -f *.aux *.log *.bbl *.blg *.idx *.ind *.lof *.lot *.out *.toc *.acn *.acr *.alg *.glg *.glo *.gls *.ist
+rm -f *.aux *.log *.bbl *.blg *.idx *.ind *.lof *.lot *.out *.toc *.acn *.acr *.alg *.glg *.glo *.gls *.ist *.spl *.fls *.fdb_latexmk *.synctex.gz *.bcf *.run.xml
 
 echo "Process completed successfully."
 echo "PDF saved to $target_folder/$output_pdf_name"
-echo "Bibliography saved to $target_folder/T-reX_references.bib"
+echo "DOCX saved to $target_folder/$output_docx_name"
+echo "HTML saved to $target_folder/$output_html_name"
 open "$target_folder/$output_pdf_name" # Open the PDF
 echo "======================================================="
